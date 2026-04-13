330b415401f876011968d8b7ac3d627fe5d7110b
Sanuka Pathiraja <sanukapathiraja@gmail.com>
2026-04-13 18:04:58 +0530

    Move MongoDB URI to environment variables

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
+
 module.exports = {
-	PORT: 5555,
-	mongoDBURL: "mongodb://sanukapathiraja_db_user:Sanuka%402006@ac-wdolyad-shard-00-00.ofq1m8w.mongodb.net:27017,ac-wdolyad-shard-00-01.ofq1m8w.mongodb.net:27017,ac-wdolyad-shard-00-02.ofq1m8w.mongodb.net:27017/?authSource=admin&replicaSet=atlas-drjoed-shard-0&tls=true&retryWrites=true&w=majority&appName=Cluster0",
+	PORT: Number(process.env.PORT) || 5555,
+	mongoDBURL: process.env.MONGODB_URL || "",
 };
